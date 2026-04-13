1857459734ea17ad9e1c544aaf133c5ba565a3aa
carol <carolina.llandres@edu.uah.es>
2026-04-13 18:37:55 +0200

    Archivos json

---
+++ src/es/uah/matcomp/mp/e2/persistencia_datos/extras/ejercicio2/Main.java
@@ -0,0 +1,36 @@
+package es.uah.matcomp.mp.e2.persistencia_datos.extras.ejercicio2;
+
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Gson gson = new Gson();
+
+        // 1. Crear el objeto interno
+        Direccion dir = new Direccion("Mayor", 12);
+
+        // 2. Crear el objeto que contiene al otro
+        Alumno alumno = new Alumno("Lucía", dir);
+
+        // 3. Guardar en JSON
+        try (FileWriter fw = new FileWriter("alumno.json")) {
+            gson.toJson(alumno, fw);
+            System.out.println("Alumno guardado en alumno.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 4. Cargar desde JSON
+        try (FileReader fr = new FileReader("alumno.json")) {
+            Alumno cargado = gson.fromJson(fr, Alumno.class);
+            System.out.println("Alumno cargado desde JSON:");
+            System.out.println(gson.toJson(cargado)); // Mostrarlo bonito
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/es/uah/matcomp/mp/e2/persistencia_datos/extras/ejercicio4/necesario/MyIterate.java
@@ -0,0 +1,7 @@
+package es.uah.matcomp.mp.e2.persistencia_datos.extras.ejercicio4.necesario;
+
+public interface MyIterate<T> {
+
+    boolean hasNext();
+    T next();
+}
